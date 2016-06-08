9ad532844988df358af88c71adcacc529008423a
Jesus Ricaño <jesusrq92@hotmail.com>
2016-06-08 17:20:19 -0500

    quite info de footer

--- js/operaciones.js
+++ js/operaciones.js
@@ -5,3 +5,20 @@
             $(this).addClass('active');                
         });            
     });*/
+
+    //Cambia a la siguiente section mostrando transición
+
+$(document).ready(function(){
+     $('a[href^="#"]').on('click',function (e) {
+         e.preventDefault();
+
+         var target = this.hash;
+         var $target = $(target);
+
+         $('html, body').stop().animate({
+             'scrollTop': $target.offset().top
+         }, 900, 'swing', function () {
+             window.location.hash = target;
+            });
+     });
+}); 
