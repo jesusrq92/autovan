af930acbf9ded0f4a9fe4802287e29a04dbc458f
Jesus Ricaño <jesusrq92@hotmail.com>
2016-06-13 17:48:23 -0500

    flecha con jquery terminada

--- js/operaciones.js
+++ js/operaciones.js
@@ -1,13 +1,5 @@
-/*$(document).ready(function () {
-        $('ul.nav > li').click(function (e) {
-            e.preventDefault();
-            $('ul.nav > li').removeClass('active');
-            $(this).addClass('active');                
-        });            
-    });*/
 
-    //Cambia a la siguiente section mostrando transición
-
+//Cambia a la siguiente section mostrando transición
 $(document).ready(function(){
      $('a[href^="#"]').on('click',function (e) {
          e.preventDefault();
@@ -22,3 +14,23 @@ $(document).ready(function(){
             });
      });
 }); 
+
+ 
+//Flecha desvanece al subir y aparece al bajar
+      $(window).on("load",function() {
+        $(window).scroll(function() {
+            $(".divFlecha").each(function() {
+              /* Check the location of each desired element */
+              var objectBottom = $(this).offset().top + $(this).outerHeight();
+              var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+      
+              /* If the element is completely within bounds of the window, fade it in */
+              if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+              if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+              } else { //object goes out of view (scrolling up)
+                      if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+              }
+            });
+        }); $(window).scroll(); //invoke scroll-handler on page-load
+      });
+
